9d882574b8702d1724894dd7dee802a93d480688
Brandon Pitts <brandomoki.bp@gmail.com>
2022-08-24 22:10:53 -0700

    added weather api

--- server.js
+++ server.js
@@ -6,18 +6,17 @@ console.log('yass');
 
 const express = require('express');
 const cors = require('cors');
-let dataJson = require('./data/weather.json');
 require('dotenv').config();
+const axios = require('axios');
 
 // once express is in we need to use it per express docs
 const app = express();
-
+const PORT = process.env.PORT || 3002;
 // middleware to share resources on the internet
 app.use(cors());
 
-const PORT = process.env.PORT || 3002;
-
-// ROUTE***************************************************************
+console.log('------------------------');
+// EndPoints***************************************************************
 
 // Base route
 app.get('/', (request, response) => {
@@ -26,30 +25,63 @@ app.get('/', (request, response) => {
 
 // get JSON(API) DATA FOR WEATHER ******************************************
 
-app.get('/weather', (request, response) => {
-  try {
-    let data = request.query.city;
-    console.log('parameters',);
-    // console.log(data);
-    let dataToGroom = dataJson.find(city => city.data === data);
-    let dataToSend = dataToGroom.data.map(object => {
-      return new Forecast(object);
-    });
+app.get('/weather', getWeather);
 
-    // console.log('New log', data[0].snow);
+async function getWeather(request, response, next) {
+  let lat = request.query.lat;
+  let lon = request.query.lon;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHERBIT_ACCESS_KEY}`;
+  console.log('************', getWeather);
+  try {
+    // AXIOS CALL *********************************************************************
+    const weatherResponse = await axios.get(url);
+
+    let dataToGroom = weatherResponse.data;
+    let dataToSend = dataToGroom.data.map(wetObj => {
+      return new Forecast(wetObj);
+
+    });
     response.status(200).send(dataToSend);
+    console.log('^^^^^^^^^^^^^^^^^^^^^', weatherResponse);
+
+    // console.log(weatherResponse);
   } catch (error) {
     next(error);
   }
 
-});
+}
 
+// app.get('/movie', getMovie);
 
+// async function getMovie(request, response, next) {
+//   let movie = request.query.lat;
+//   const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHERBIT_ACCESS_KEY}`;
+//   console.log('************', getWeather);
+//   try {
+//     // AXIOS CALL *********************************************************************
+//     const weatherResponse = await axios.get(url);
 
+//     let dataToGroom = weatherResponse.data;
+//     let dataToSend = dataToGroom.data.map(wetObj => {
+//       return new Forecast(wetObj);
+
+//     });
+//     response.status(200).send(dataToSend);
+//     console.log('^^^^^^^^^^^^^^^^^^^^^', weatherResponse);
+
+//     // console.log(weatherResponse);
+//   } catch (error) {
+//     next(error);
+//   }
+
+// }
+
+//**************CLASS CONSTRUCTOR  *********************************************************/
 class Forecast {
   constructor(wetObj){
     this.date = wetObj.valid_date;
     this.description = wetObj.weather.description;
+    this.temp = wetObj.temp;
     this.lon = wetObj.lon;
     this.lat = wetObj.lat;
   }
@@ -60,7 +92,7 @@ app.get('*', (request, response) => {
   response.status(404).send('The path you have chosen does not exist');
 });
 
-app.use((error, request, response, next)=> {
+app.use((error, request, response)=> {
   response.status(500).send(error.message);
 });
 
